1dda14bc14c44a3beda1a4584bde4e1f405e44fa
Jason Shiuan <jfshiuan@ucsd.edu>
2016-02-25 01:54:40 -0800

    thirdpartyAPIlab

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,15 +1,15 @@
 function initCamera() {
   //Uncomment and fill in the correct selectors below.
-  //capture($('<Your code here>'),
-  //        $('<Your code here>'),
-  //        $('<Your code here>'));
+  capture($('#camera-video'),
+   $('#camera-canvas'),
+   $('#camera-button'));
 }
 
 function capture(video, canvas, snapshotButton) {
   //Adopted from https://dev.opera.com/articles/media-capture-in-mobile-browsers/
   //Setup navigator for all versions of browsers.
   navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-              navigator.mozGetUserMedia || navigator.msGetUserMedia;
+  navigator.mozGetUserMedia || navigator.msGetUserMedia;
   var ctx = canvas[0].getContext('2d');
 
   var successCallback = function(mediaStream) {
@@ -17,14 +17,14 @@ function capture(video, canvas, snapshotButton) {
     //draw the image on the canvas.
     video.attr('src', window.URL.createObjectURL(mediaStream));
     snapshotButton.click(function(e) {
-        console.log("Taking photo");
+      console.log("Taking photo");
         //Calculate dimension of photo from the video element.
         var width = video.width();
         var height = video.height();
         canvas.attr('width', width);
         canvas.attr('height', height);
         ctx.drawImage(video[0], 0, 0, width, height);
-    });
+      });
   };
 
   var errorCallback = function() {
@@ -35,7 +35,7 @@ function capture(video, canvas, snapshotButton) {
 
   //Register the success and error callbacks with getUserMedia.
   navigator.getUserMedia({ 'video': true },
-      successCallback, errorCallback);
+    successCallback, errorCallback);
 
 };
 
